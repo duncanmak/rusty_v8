4e7a6901ff9ed1145c4bc12646d5a1db394e4407
Duncan Mak <duncanmak@gmail.com>
2025-09-10 16:31:12 -0400

    Fix lessmsi call

--- tools/prepare_arm64.ps1
+++ tools/prepare_arm64.ps1
@@ -22,9 +22,10 @@ function Main {
     &"choco" install lessmsi -y
 
     # Extract dbghelp.dll from the MSI
-    &"lessmsi" "x" "$driveLetter\Installers\X64 Debuggers And Tools-x64_en-us.msi" `
-               "c:\Programs Files (x86)\Windows Kits\10\Debuggers\x64\"  `
-               "SourceDir\Windows Kits\10\Debuggers\x64\dbghelp.dll"
+    &"lessmsi" "x" "$driveLetter\Installers\X64 Debuggers And Tools-x64_en-us.msi" "$env:TTEMP\"
+
+    Copy-Item -Path "$env:TEMP\SourceDir\Windows Kits\10\Debuggers\x64\dbghelp.dll"
+              -Destination "c:\Programs Files (x86)\Windows Kits\10\Debuggers\x64\dbghelp.dll"
 
     Dismount-DiskImage -ImagePath $isoPath
 }
