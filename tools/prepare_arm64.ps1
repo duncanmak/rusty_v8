45c2ea8782941884473b8ecfadc6617f559fd9ce
Duncan Mak <duncanmak@gmail.com>
2025-09-10 20:03:07 -0400

    Add more logging to MSI extraction

--- tools/prepare_arm64.ps1
+++ tools/prepare_arm64.ps1
@@ -1,6 +1,8 @@
 $isoPath = "$env:TEMP\winsdk.iso"
 
 function Get-ISO {
+    Write-Host "Downloading and mounting Windows SDK ISO..."
+
     # Keep in sync with https://github.com/actions/partner-runner-images/blob/main/images/arm-windows-11-image.md
     # Windows SDK for Windows 11 (10.0.26100.4654)
     $isoUrl = "https://go.microsoft.com/fwlink/?linkid=2326092"
@@ -16,19 +18,30 @@ function Get-ISO {
 }
 
 function Main {
-    $driveLetter = Get-ISO
+    try {
+        $driveLetter = Get-ISO
 
-    # install lessmsi using chocolatey
-    &"choco" install lessmsi -y
+        # install lessmsi using chocolatey
+        &"choco" "install" "lessmsi" "-y"
 
-    # Extract dbghelp.dll from the MSI
-    &"lessmsi" "x" "$driveLetter\Installers\X64 Debuggers And Tools-x64_en-us.msi" "$env:TEMP\"
+        # Extract dbghelp.dll from the MSI
+        &"lessmsi" "x" "$driveLetter\Installers\X64 Debuggers And Tools-x64_en-us.msi" "$env:TEMP\"
 
-    $source = "$env:TEMP\SourceDir\Windows Kits\10\Debuggers\x64\dbghelp.dll"
-    $destination = "c:\Programs Files (x86)\Windows Kits\10\Debuggers\x64\"
-    Copy-Item -Path $source -Destination (New-Item -type Directory -Force $destination)
+        # Copy dbghelp.dll to the destination
+        $source = "$env:TEMP\SourceDir\Windows Kits\10\Debuggers\x64\dbghelp.dll"
+        $destination = "c:\Programs Files (x86)\Windows Kits\10\Debuggers\x64\"
 
-    Dismount-DiskImage -ImagePath $isoPath
+        Copy-Item -Path $source -Destination (New-Item -type Directory -Force $destination) -Verbose
+
+        # Assert that dbghelp.dll is in the right place
+        if (-not (Test-Path "C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\dbghelp.dll")) {
+            Write-Error "dbghelp.dll does not exist for x64."
+            exit 1
+        }
+    }
+    finally {
+        Dismount-DiskImage -ImagePath $isoPath
+    }
 }
 
 Main
